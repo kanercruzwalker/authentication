50ff210d0654243995c1abf6a37f1b370088b93b
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-07-25 00:57:13 -0700

    passport works on login, session is help and data persists until browser closes

--- app.js
+++ app.js
@@ -61,6 +61,20 @@ app.route("/login")
 })
 .post(function(req, res){
     
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    })
 
 });
 
